dc8997107dfa61d6cd90040e1a2a70a1d44d95b0
Teemu Penttinen <teemu.penttinen@reaktor.com>
2022-10-07 21:24:44 +0300

    feat: move view back button to app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,26 @@
 import type { AppProps } from 'next/app'
-import ViewTransition from '../components/animations/ViewTransition'
+import { useRouter } from 'next/router'
 
-import '../styles/app.css'
+import ViewTransition from '../components/animations/ViewTransition'
+import { Footer } from '../components/Footer'
+import { backButtonStyle } from '../styles/app.css'
+import { useViews } from '../utils/useViews'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const { relativePath } = useViews()
+  const { back } = useRouter()
+
   return (
-    <ViewTransition>
-      <Component {...pageProps} />
-    </ViewTransition>
+    <>
+      {relativePath && (
+        <button className={backButtonStyle} onClick={back}>
+          Back
+        </button>
+      )}
+      <ViewTransition>
+        <Component {...pageProps} />
+      </ViewTransition>
+      <Footer />
+    </>
   )
 }
