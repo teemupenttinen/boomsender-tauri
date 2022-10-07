3eac1d2297f89a45c6a5be3420b5127707799926
Teemu Penttinen <teemu.penttinen@reaktor.com>
2022-10-07 21:28:15 +0300

    refactor: remove back button from BaseView

--- src/components/BaseView.css.ts
+++ src/components/BaseView.css.ts
@@ -4,10 +4,6 @@ export const baseViewStyle = style({
   padding: '8px',
 })
 
-export const backButtonStyle = style({
-  color: 'white',
-})
-
 export const baseViewTopContainer = style({
   height: '10px',
 })
--- src/components/BaseView.tsx
+++ src/components/BaseView.tsx
@@ -1,20 +1,11 @@
-import { assignInlineVars } from '@vanilla-extract/dynamic'
-import { useRouter } from 'next/router'
-import { PropsWithChildren, useMemo } from 'react'
-import { backButtonStyle, baseViewStyle, baseViewTopContainer } from './BaseView.css'
+import { PropsWithChildren } from 'react'
+
+import { baseViewStyle, baseViewTopContainer } from './BaseView.css'
 
 export const BaseView: React.FC<PropsWithChildren> = ({ children }) => {
-  const { asPath, back } = useRouter()
-  const showBack = useMemo(() => asPath !== '/', [asPath])
   return (
     <div className={baseViewStyle}>
-      <div className={baseViewTopContainer}>
-        {showBack && (
-          <button className={backButtonStyle} onClick={back}>
-            Back
-          </button>
-        )}
-      </div>
+      <div className={baseViewTopContainer}></div>
       {children}
     </div>
   )
