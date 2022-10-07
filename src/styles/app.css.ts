dded8d1f8424f04a6485d1648304299157a216fa
Teemu Penttinen <teemu.penttinen@reaktor.com>
2022-10-07 21:27:38 +0300

    chore: update app css

--- src/styles/app.css.ts
+++ src/styles/app.css.ts
@@ -2,14 +2,21 @@ import { globalStyle, style } from '@vanilla-extract/css'
 
 export const base = style({ margin: 12 })
 
+export const backButtonStyle = style({
+  color: 'white',
+})
+
 globalStyle('body', {
   background: '#242331',
   fontFamily: 'Helvetica',
   margin: 0,
+  overflow: 'hidden',
 })
 
 globalStyle('input', {
   WebkitAppearance: 'none',
+  margin: '0',
+  padding: '0',
 })
 
 globalStyle('button', {
